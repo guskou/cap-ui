47802d4b56342942781f88a29eca733e1625a7a0
breadthe <constantin.cojocaru@breaktech.com>
2017-03-16 22:43:57 -0500

    Dashboard skeleton.

---
+++ tests/integration/components/packets-card-test.js
@@ -0,0 +1,24 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('packets-card', 'Integration | Component | packets card', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{packets-card}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#packets-card}}
+      template block text
+    {{/packets-card}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
